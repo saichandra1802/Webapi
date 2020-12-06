c32d412bd503cb1b7f20897f28b1459e32f756c4
saichandra datrika <datrikasaichandra@gmail.com>
2020-12-06 21:30:37 +0530

    third commit

--- WebApplication9/Controllers/HomeController.cs
+++ WebApplication9/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication9.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page1";
+            ViewBag.Title = "Home Page  1";
 
             return View();
         }
