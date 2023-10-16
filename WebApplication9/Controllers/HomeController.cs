6158795efa8391b4dcc925debcc19657a9f0c5f2
saichandra <datrikasaichandra@gmail.com>
2023-10-16 09:24:06 +0530

    #12345 login task

--- WebApplication9/Controllers/HomeController.cs
+++ WebApplication9/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace WebApplication9.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page 1 GIt UI ";
+            ViewBag.Title = "Home Page 1 GIt UI ";
             ViewBag.Title = "Home Page 1 GIt UI " ;
             ViewBag.Title = "Home Page 2 GIt UI ";
             ViewBag.Title = "Home Page 1 GIt UI ";
