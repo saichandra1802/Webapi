5fe580db289dddeb78d4eab80064eefead3f6a5e
saichandra <datrikasaichandra@gmail.com>
2024-07-09 23:00:10 +0530

    #12544 signupDevelop task

--- WebApplication9/Controllers/HomeController.cs
+++ WebApplication9/Controllers/HomeController.cs
@@ -10,6 +10,9 @@ namespace WebApplication9.Controllers
     {
         public ActionResult Index()
         {
+
+            ViewBag.Title = "sd";
+
             ViewBag.Title = "signup Task";
 
             ViewBag.Title = "sample Task";
